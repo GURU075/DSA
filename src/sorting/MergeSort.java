4cf6f8bd3d71fd42688a9e99c87eb82b50626af7
Gururaj Yadav <gururajyadav.07@gmail.com>
2026-06-29 15:22:49 +0530

    refactor Merge Sort implementation for improved readability and efficiency

--- src/sorting/MergeSort.java
+++ src/sorting/MergeSort.java
@@ -8,42 +8,39 @@ public class MergeSort{
             System.out.print(n+" ");
         }
     }
-    static void sortArray(int[] nums){
-
+  static void sortArray(int[] nums){
         divide(0,nums.length-1,nums);
-
-    }
-    static void divide(int s,int e,int[] nums){
+  }
+  static void divide(int s,int e,int[] nums){
         if(s>=e){
             return;
         }
         int mid = s+(e-s)/2;
         divide(s,mid,nums);
         divide(mid+1,e,nums);
-        conquer(nums,s,e,mid);
-    }
-    static void conquer(int[] nums,int s ,int e,int mid){
-
+        conquer(s,mid,e,nums);
+  }
+  static void conquer(int s,int mid,int e, int[] nums){
         int idx1= s;
         int idx2= mid+1;
         int[] ans = new int[e-s+1];
-        int i=0;
+        int idx=0;
         while(idx1<=mid && idx2<=e){
             if(nums[idx1]<=nums[idx2]){
-                ans[i++] = nums[idx1++];
+                ans[idx++] = nums[idx1++];
             }else{
-                ans[i++] = nums[idx2++];
+                ans[idx++] = nums[idx2++];
             }
         }
         while(idx1<=mid){
-            ans[i++] = nums[idx1++];
+            ans[idx++] = nums[idx1++];
         }
-        while(idx2<=e){
-            ans[i++] = nums[idx2++];
-        }
-        for(int id=0,j=s;j<=e;id++,j++){
-            nums[j]=ans[id];
-        }
-    }
+      while(idx2<=e){
+          ans[idx++] = nums[idx2++];
+      }
+      for(int i=s,j=0;i<=e;i++,j++){
+          nums[i] = ans[j];
+      }
+  }
 
 }
